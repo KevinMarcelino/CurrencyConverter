51d81a9a4799a63b10403242bd8a821861ab7a88
Mony Kaushik <mkau5855@uni.sydney.edu.au>
2019-09-18 18:23:01 +1000

    Fixed Import Statements

--- src/main/java/CurrencyConverter/App.java
+++ src/main/java/CurrencyConverter/App.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+package CurrencyConverter;
+
+import java.util.*;
 
 public class App {
     static Map<String, String> UsernamesAndPasswords = new HashMap<String, String>();
