62ef5c2ea18153b183c2fe95e206f39568f50419
Woojin Lee <wlee2926@uni.sydney.edu.au>
2019-09-18 20:15:39 +1000

    Fixed errors
    Errors fixed on sum tests
    More tests added for driver

--- src/test/java/CurrencyConverter/AppTest.java
+++ src/test/java/CurrencyConverter/AppTest.java
@@ -83,6 +83,7 @@ class AppTest {
         assertEquals(App.fromCurrency(), null);
     }
 
+
     @Test
     void testToCurrency1() {
         String data = "aud";
@@ -98,7 +99,7 @@ class AppTest {
         InputStream stdin = System.in;
         System.setIn(new ByteArrayInputStream(data.getBytes()));
         App.input = new Scanner(System.in);
-        assertEquals(null, App.fromCurrency());
+        assertEquals(App.fromCurrency(), null);
     }
 
     @Test
@@ -108,17 +109,17 @@ class AppTest {
         System.setIn(new ByteArrayInputStream(data1.getBytes()));
         App.input = new Scanner(System.in);
         App.sum();
-        assertEquals("How many currencies you want to sum up? " + System.getProperty("line.separator") +
-                "The maximum number you can sum is 3\nPlease enter again" + System.getProperty("line.separator") +
-                "You have entered an invalid number\nPlease try again" + System.getProperty("line.separator") +
-                "Enter the 1 currency and the amount" + System.getProperty("line.separator") +
-                "Enter the 2 currency and the amount" + System.getProperty("line.separator") +
-                "Enter the 3 currency and the amount" + System.getProperty("line.separator") +
-                "Invalid input, Please provide with a Currency and Amount!" + System.getProperty("line.separator") +
-                "Enter the 3 currency and the amount" + System.getProperty("line.separator") +
-                "Enter the currency would like to convert to:" + System.getProperty("line.separator") +
-                "Select To Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.getProperty("line.separator") +
-                "Answer is 4.87" + System.getProperty("line.separator"), outContent.toString());
+
+        assertEquals("How many currencies you want to sum up? " + System.lineSeparator() +
+                "The maximum number you can sum is 3\nPlease enter again" + System.lineSeparator() +
+                "You have entered an invalid number\nPlease try again" + System.lineSeparator() +
+                "Enter the 1 currency and the amount" + System.lineSeparator() +
+                "Enter the 2 currency and the amount" + System.lineSeparator() +
+                "Enter the 3 currency and the amount" + System.lineSeparator() +
+                "Invalid input, Please provide with a Currency and Amount!" + System.lineSeparator() +
+                "Enter the 3 currency and the amount" + System.lineSeparator() +
+                "Enter the currency would like to convert to:\nSelect To Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "Answer is 4.87\r\n", outContent.toString());
     }
 
     @Test
@@ -128,7 +129,8 @@ class AppTest {
         System.setIn(new ByteArrayInputStream(data1.getBytes()));
         App.input = new Scanner(System.in);
         App.sum();
-        assertEquals("How many currencies you want to sum up? " + System.lineSeparator() +
+
+        assertEquals( "How many currencies you want to sum up? " + System.lineSeparator() +
                 "You must input a number\nPlease try again" + System.lineSeparator() +
                 "Enter the 1 currency and the amount" + System.lineSeparator() +
                 "Invalid input, Please provide with a Currency and Amount!" + System.lineSeparator() +
@@ -141,9 +143,8 @@ class AppTest {
                 "Enter the 2 currency and the amount" + System.lineSeparator() +
                 "input must be a number" + System.lineSeparator() +
                 "Enter the 2 currency and the amount" + System.lineSeparator() +
-                "Enter the currency would like to convert to:" + System.lineSeparator() +
-                "Select To Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
-                "Answer is 307.0" + System.lineSeparator(), outContent.toString());
+                "Enter the currency would like to convert to:\nSelect To Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "Answer is 307.0" + System.getProperty("line.separator"), outContent.toString());
     }
 
 
@@ -280,7 +281,7 @@ class AppTest {
         assertEquals(62.0, App.convert(aud, eur, 100.0));
     }
 
-    @Test
+    @Test //aud to usd
     void testingDriver() {
         String data1 = "convert\naud\nusd\n10\nN";
         InputStream stdin = System.in;
@@ -293,8 +294,69 @@ class AppTest {
                 "How much AUD you want to convert to USD?" + System.lineSeparator() +
                 "10.00 AUD is 6.90 USD to!\n" + System.lineSeparator() +
                 "Would you like to make another conversion or Sum? Y or N" + System.lineSeparator(), outContent.toString());
-
     }
+
+    @Test  //aud to euro
+    void testingDriver2() {
+        String data1 = "convert\naud\neuro\n10\nN";
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream(data1.getBytes()));
+        App.input = new Scanner(System.in);
+        App.driver();
+        assertEquals("Would you like to convert or sum the money? Enter exit to exit." + System.lineSeparator() +
+                "Select From Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "Select To Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "How much AUD you want to convert to EURO?" + System.lineSeparator() +
+                "10.00 AUD is 6.20 EURO to!\n" + System.lineSeparator() +
+                "Would you like to make another conversion or Sum? Y or N" + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test  //aud to pound
+    void testingDriver3() {
+        String data1 = "convert\naud\npound\n10\nN";
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream(data1.getBytes()));
+        App.input = new Scanner(System.in);
+        App.driver();
+        assertEquals("Would you like to convert or sum the money? Enter exit to exit." + System.lineSeparator() +
+                "Select From Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "Select To Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "How much AUD you want to convert to POUND?" + System.lineSeparator() +
+                "10.00 AUD is 5.60 POUND to!\n" + System.lineSeparator() +
+                "Would you like to make another conversion or Sum? Y or N" + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test  //aud to sgd
+    void testingDriver4() {
+        String data1 = "convert\naud\nsgd\n10\nN";
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream(data1.getBytes()));
+        App.input = new Scanner(System.in);
+        App.driver();
+        assertEquals("Would you like to convert or sum the money? Enter exit to exit." + System.lineSeparator() +
+                "Select From Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "Select To Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "How much AUD you want to convert to SGD?" + System.lineSeparator() +
+                "10.00 AUD is 9.50 SGD to!\n" + System.lineSeparator() +
+                "Would you like to make another conversion or Sum? Y or N" + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test  //aud to sgd
+    void testingDriverExit() {
+        String data1 = "convert\naud\nsgd\n10\nY\nexit";
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream(data1.getBytes()));
+        App.input = new Scanner(System.in);
+        App.driver();
+        assertEquals("Would you like to convert or sum the money? Enter exit to exit." + System.lineSeparator() +
+                "Select From Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "Select To Currency: (USD, AUD, EURO, POUND, SGD)[CASE INSENSITIVE] " + System.lineSeparator() +
+                "How much AUD you want to convert to SGD?" + System.lineSeparator() +
+                "10.00 AUD is 9.50 SGD to!\n" + System.lineSeparator() +
+                "Would you like to make another conversion or Sum? Y or N" + System.lineSeparator() +
+                "Would you like to convert or sum the money? Enter exit to exit." +System.lineSeparator(), outContent.toString());
+    }
+
 
 }
 
